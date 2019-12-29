31042666084cf00a89c060a495d32fca641c4fe8
Velitchko Filipov <velitchkofilipov@gmail.com>
2019-12-29 15:37:09 +0100

    imported: ngx-electron package for angular (usable as service in components now)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// Angular Electron Module
+import { NgxElectronModule } from 'ngx-electron';
 
 @NgModule({
   declarations: [
@@ -10,6 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    NgxElectronModule,
     AppRoutingModule
   ],
   providers: [],
